9c96ef5d21c527a66c73c8120a395e1fc5be8b60
ph4n <yt.ph4nt0m7@gmail.com>
2019-09-04 21:03:52 +0300

    ww

---
+++ idk/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  idk
+//
+//  Created by Kostya on 04/09/2019.
+//  Copyright © 2019 ph4nt0m7. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
